696b93b3a89cd0315e0a45f90ca5ccf5469777c3
Luis Escobar <ljmatias@ucsd.edu>
2020-02-18 10:44:01 -0800

    Added features

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeCtr").click(sendData);
+	console.log("Button hit");
+}
+
+function sendData(){
+	ga("send", "event", 'like', 'click');
+	console.log("data sent");
 }
